b6d961e46bcba4dcbc98ca1e8354a3b3391cd9be
Shane D Wilson <me@shanedwilson.com>
2019-03-01 11:34:03 -0600

    Add project files.

---
+++ StringAndNumberManipulation/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace StringAndNumberManipulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
